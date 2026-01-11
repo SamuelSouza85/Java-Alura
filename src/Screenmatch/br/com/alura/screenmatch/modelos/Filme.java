3ba53099520d93dacd99db1684b62b9c0f1f9c27
SamuelSouza85 <samueldesouza859@gmail.com>
2026-01-10 22:01:41 -0300

    Adding files in archive Screenmatch

--- src/Screenmatch/br/com/alura/screenmatch/modelos/Filme.java
+++ src/Screenmatch/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,93 +1,13 @@
 package Screenmatch.br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private  String nome;
-    private  String genero;
-    private int anoDeLancamento;
-    private  boolean inclusoNoPlano;
-    private double somaDasAvaliacao;
-    private int totalAvaliacoes;
-    private  int duracaoEmMinutos;
-    private  boolean dublado;
+public class Filme extends Titulo {
+        private String diretor;
 
-
-    public int getTotalAvaliacoes() {
-        return totalAvaliacoes;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setInclusoNoPlano(boolean inclusoNoPlano) {
-        this.inclusoNoPlano = inclusoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void setDublado(boolean dublado) {
-        this.dublado = dublado;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isInclusoNoPlano() {
-        return inclusoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public boolean isDublado() {
-        return dublado;
-    }
-
-    public void imprime(){
-        System.out.println("Nome do Filme: " + nome);
-        System.out.println("Gênero do File: " + genero);
-        System.out.println("Ano de Lançamento do filme: " + anoDeLancamento);
-        if (inclusoNoPlano){
-            System.out.println("O filme está incluso no plano");
-        }else{
-            System.out.println("O filme não está incluso no plano");
-        }
-        System.out.println("A avalição do filme é de: " + somaDasAvaliacao);
-        System.out.println("O total de avaliações do filme é: " + totalAvaliacoes);
-        System.out.println("O filme tem um total de " + duracaoEmMinutos+ " minutos de duração");
-        if (dublado){
-            System.out.println("O filme possui uma versão dublada");
-        }else {
-            System.out.println("O filme não possui uma versão dublada");
-        }
-    }
-
-    public void  avalia(double nota){
-            somaDasAvaliacao += nota;
-            totalAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacao / totalAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
